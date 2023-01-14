f2132b675a214ef408d209108f7c1c3ff0473957
OmegaSunkey <bronytecfan@gmail.com>
2023-01-14 15:38:45 -0500

    will this crash

--- UserBG/index.ts
+++ UserBG/index.ts
@@ -5,29 +5,31 @@ import { after } from "aliucord/utils/patcher";
 
 export default class UserBG extends Plugin {
     public async start() {
-      //let userid;
-      //const datab = fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json").then((response) => response.json());
+      let userid;
+      const datab = JSON.parse(fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json"));
         const ProfileBanner = getByName("ProfileBanner"); //thank you cloudburst https://github.com/c10udburst-discord/Aliucord-RightNow-Plugins 
         after(ProfileBanner, "default", (ctx, component) => {
             const [{bannerSource}] = ctx.args;
 
-            const bnurl = bannerSource.uri
-                .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
-                .replace('.webp', '.png');
+            const bnurl = bannerSource.uri;
                 
-               /* if(bnurl === null) {
-                  
-                } */
+                if(bnurl === null) {
+                  try {
+                    if(userid == datab.uid) {
+                      bnurl = datab.img;
+                    } 
+                  } catch(e) {
+                    this.logger.info("Wrong wrong" + e);
+                  } 
+                } 
             this.logger.info("Banner URi " + bnurl);
-            this.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
+            //this.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
         });
         
         const HeaderAvatar = getByName("HeaderAvatar");
         after(HeaderAvatar, "default", (ctx, component) => {
           const [{user, style}] = ctx.args;
-          
-          this.logger.info("What is this " + Object.values(user));
-          this.logger.info("Will this error " + Object.values(user)[4]);
+          userid = Object.values(user)[3]; //the most cursed way to get an id 
         });
     }
 }
