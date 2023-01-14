f5d16fbfdac887f3979dbf9b3cbf62a68190e384
OmegaSunkey <bronytecfan@gmail.com>
2023-01-14 14:52:17 -0500

    add: trying to get id in weird way

--- UserBG/index.ts
+++ UserBG/index.ts
@@ -5,15 +5,28 @@ import { after } from "aliucord/utils/patcher";
 
 export default class UserBG extends Plugin {
     public async start() {
+      //let userid;
+      //const datab = fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json").then((response) => response.json());
         const ProfileBanner = getByName("ProfileBanner"); //thank you cloudburst https://github.com/c10udburst-discord/Aliucord-RightNow-Plugins 
         after(ProfileBanner, "default", (ctx, component) => {
             const [{bannerSource}] = ctx.args;
 
-            const url = bannerSource.uri
+            const bnurl = bannerSource.uri
                 .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
                 .replace('.webp', '.png');
+                
+               /* if(bnurl === null) {
+                  
+                } */
             this.logger.info("Banner URi " + url);
             this.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
+        });
+        
+        const HeaderAvatar = getByName("HeaderAvatar");
+        after(HeaderAvatar, "default", (ctx, component) => {
+          const [{avatarsrc}] = ctx.args;
+          
+          this.logger.info("What is this " + Object.values(avatarsrc));
         });
     }
 }
