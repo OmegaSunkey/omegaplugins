6390f364c78ebc6461821b72104843676cb3eb1e
OmegaSunkey <bronytecfan@gmail.com>
2023-01-14 16:27:01 -0500

    add: ok i will regex

--- UserBG/index.ts
+++ UserBG/index.ts
@@ -6,8 +6,15 @@ import { after } from "aliucord/utils/patcher";
 export default class UserBG extends Plugin {
     public async start() {
       let userid;
-      const datab = fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json").then((response) => response.json());
+      const regex = .*?\"(http?s:\/\/[\w.\/-]*)\";
+      const datab = fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json").then((response) => response.text());
       
+        const HeaderAvatar = getByName("HeaderAvatar");
+        after(HeaderAvatar, "default", (ctx, component) => {
+          const [{user, style}] = ctx.args;
+          userid = Object.values(user)[3]; //the most cursed way to get an id 
+        }); 
+        
         const ProfileBanner = getByName("ProfileBanner"); //thank you cloudburst https://github.com/c10udburst-discord/Aliucord-RightNow-Plugins 
         after(ProfileBanner, "default", (ctx, component) => {
             const [{bannerSource}] = ctx.args;
@@ -16,9 +23,10 @@ export default class UserBG extends Plugin {
                 
                 if(bnurl === null) {
                   try {
-                    if(userid == datab.uid) {
-                      bnurl = datab.img;
-                    } 
+                    theimg = datab.match(userid + regex);
+                    this.logger.info("Custom Img " + theimg[1]);
+                    this.logger.info("User id " + userid);
+                    bnurl = theimg[1];
                   } catch(e) {
                     this.logger.info("Wrong wrong" + e);
                   } 
@@ -27,10 +35,5 @@ export default class UserBG extends Plugin {
             //this.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
         });
         
-        const HeaderAvatar = getByName("HeaderAvatar");
-        after(HeaderAvatar, "default", (ctx, component) => {
-          const [{user, style}] = ctx.args;
-          userid = Object.values(user)[3]; //the most cursed way to get an id 
-        });
     }
 }
