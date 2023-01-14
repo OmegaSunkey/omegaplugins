d6eed53e4620574fa54ff082b6c38bea4c78db0a
OmegaSunkey <bronytecfan@gmail.com>
2023-01-14 17:54:50 -0500

    will this finally work

--- UserBG/index.ts
+++ UserBG/index.ts
@@ -10,8 +10,7 @@ export default class UserBG extends Plugin {
       const response = await fetch("https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json");
       //this.logger.info("database " + datab);
       const datab = await response.text();
-      let theimg = datab.match(userid + regex); 
-      this.logger.info("match " + theimg);
+      //his.logger.info("match " + theimg);
       
       const HeaderAvatar = getByName("HeaderAvatar");
       after(HeaderAvatar, "default", (ctx, component) => {
@@ -27,6 +26,8 @@ export default class UserBG extends Plugin {
                 
                 if(bannerSource == null) {
                   try {
+                    const join = userid + regex;
+                    let theimg = datab.match(join);
                     this.logger.info("Custom Img " + theimg[1]);
                     this.logger.info("User id " + userid);
                     bannerSource = theimg[1];
@@ -35,7 +36,7 @@ export default class UserBG extends Plugin {
                   } 
                 } 
             //this.logger.info("Banner URi " + bnurl);
-            this.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
+            //his.logger.info("Banner ctxargs " + Object.values(bannerSource)) 
         });
         
     }
