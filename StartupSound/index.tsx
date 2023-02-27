e043d28c30e151712b58c77c11d4e3851b37a471
OmegaSunkey <bronytecfan@gmail.com>
2023-02-26 19:13:58 -0500

    hard music

--- StartupSound/index.tsx
+++ StartupSound/index.tsx
@@ -4,6 +4,10 @@ import { getByProps, React } from "aliucord/metro";
 export default class StartupSound extends Plugin {
     public async start() {
       const Video = getByProps("DRMType");
-      <Video source={{uri: "https://discord.com/assets/ae7d16bb2eea76b9b9977db0fad66658.mp3"}} audioOnly/>
+      return <>
+      	<Video source={{ uri: "https://github.com/FierysDiscordAddons/Moyai/raw/main/src/boom.mp4"}}
+      		paused={false} 
+      		audioOnly={true}/>
+      	</>
     }
 } 
